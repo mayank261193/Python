cc29a0b21aad63224ce8c99c1f3644fc673368a4
Mayank goyel <mayank261193@gmail.com>
2026-07-22 18:07:14 +0530

    Improve username and initials generation logic
    Refactor username generation code for clarity and consistency.

--- drill_03_username.py
+++ drill_03_username.py
@@ -4,12 +4,12 @@ full_name = "  asha   verma  "
 birth_year = 1999
 
 # YOUR CODE HERE
-Clean=full_name.strip().title()
-Name=Clean.replace("","")
-Parts=Name.split()
-Initials=Parts[0][0]+"."+Parts[1][0]+"."
-print(Initials)
-Username=Parts[0]+str(birth_year)[-2:]
+Clean=full_name.strip().title()           #Assign the value to clean and removing spaces.
+Name=Clean.replace("","")                 #Assign Name and removing middle spaces.
+Parts=Name.split()                        #Split the Name into parts like array.
+Initials=Parts[0][0]+"."+Parts[1][0]+"."  #Calling of particular values.
+print(Initials)# Print initials.
+Username=Parts[0]+str(birth_year)[-2:]    #Creating username by calling particular part using concatenation
 print(Username)
 
 # Expected:
